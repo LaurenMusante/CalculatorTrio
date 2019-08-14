010916dd909e2591c1e008b361536793f0943e13
Jacqueline Remmel, lauren musante and sofia giordano <jacqueline@>
2019-08-14 14:18:52 -0700

    Add output div in HTML where result of addition is displayed.

--- js/scripts.js
+++ js/scripts.js
@@ -17,8 +17,14 @@ var divide = function(number7, number8) {
 
 // front end logic stuff:
 $(document).ready(function() {
-  var userNumber1 = parseInt($("#add1").val());
-  var userNumber2 = parseInt($("#add2").val());
+  $("form#add").submit(function(event) {
 
-  alert(add(number1, number2));
+    var userNumber1 = parseInt($("#add1").val());
+    var userNumber2 = parseInt($("#add2").val());
+
+    var addResult = add(userNumber1, userNumber2);
+    $("#output").text(addResult);
+
+    event.preventDefault();
+  });
 });
