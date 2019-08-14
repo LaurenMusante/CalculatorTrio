bf7ee7a529ea872ee676080665a1f2c607ea24b3
Jacqueline Remmel, lauren musante and sofia giordano <sofia@>
2019-08-14 14:01:07 -0700

    Update JS to pull values entered by the user into the form.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,24 @@
-// front end logic stuff:
-$(document).ready(function() {
-  var number1 = parseInt(prompt("Enter a number:"));
-  var number2 = parseInt(prompt("Enter another number:"));
-
-  alert(add(number1, number2));
-});
 // business logic stuff:
 var add = function(number1, number2) {
   return number1 + number2;
 };
 
-var subtract = function(number1, number2) {
-  return number1 - number2;
+var subtract = function(number3, number4) {
+  return number3 - number4;
 };
 
-var multiply = function(number1, number2) {
-  return number1 * number2;
+var multiply = function(number5, number6) {
+  return number5 * number6;
 };
 
-var divide = function(number1, number2) {
-  return number1 / number2;
+var divide = function(number7, number8) {
+  return number7 / number8;
 };
+
+// front end logic stuff:
+$(document).ready(function() {
+  var userNumber1 = parseInt($("#add1").val());
+  var userNumber2 = parseInt($("#add2").val());
+
+  alert(add(number1, number2));
+});
