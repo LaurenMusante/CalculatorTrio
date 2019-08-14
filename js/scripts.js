271c52b5cf42a8eb0c8ef811d19aaef98422f2ba
Jacqueline Remmel, lauren musante and sofia giordano <lauren@>
2019-08-14 13:40:51 -0700

    Set up initial project files with HTML and JS.

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+// front end logic stuff:
+$(document).ready(function() {
+  var number1 = parseInt(prompt("Enter a number:"));
+  var number2 = parseInt(prompt("Enter another number:"));
+
+  alert(add(number1, number2));
+});
+// business logic stuff:
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
